076e422f16b62fa9776e4c33c7b149a3237d0642
Prateek Panwar <prateekpanwar31@gmail.com>
2018-09-18 00:26:15 -0400

    Naive Bayes Model

--- ML models/NaiveBayes.py
+++ ML models/NaiveBayes.py
@@ -18,5 +18,44 @@ from sklearn import metrics
 from sklearn.metrics import accuracy_score
 from sklearn.cross_validation import train_test_split
 
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data"
-raw_data = urllib.urlopen(url)
+dataset = pd.read_csv('spambase.data')
+
+
+#dataset = np.loadtxt(raw_data, delimiter=",")
+
+X = dataset.iloc[:, 0:dataset.columns.size-1]
+y = dataset.iloc[:,-1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.33, random_state=17)
+
+BernNB = BernoulliNB(binarize=True)
+BernNB.fit(X_train, y_train)
+print(BernNB)
+
+y_expect = y_test
+y_pred = BernNB.predict(X_test)
+print (accuracy_score(y_expect, y_pred))
+
+MultiNB = MultinomialNB()
+
+MultiNB.fit(X_train, y_train)
+print(MultiNB)
+
+y_pred = MultiNB.predict(X_test)
+print (accuracy_score(y_expect, y_pred))
+
+GausNB = GaussianNB()
+GausNB.fit(X_train, y_train)
+print(GausNB)
+
+y_pred = GausNB.predict(X_test)
+print (accuracy_score(y_expect, y_pred))
+
+####################trying to improve the accuracy/try and error##########################
+BernNB = BernoulliNB(binarize=0.1)
+BernNB.fit(X_train, y_train)
+print(BernNB)
+
+y_expect = y_test
+y_pred = BernNB.predict(X_test)
+print (accuracy_score(y_expect, y_pred))
