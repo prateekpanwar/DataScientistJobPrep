9197b00c40d1d3aac31001cc4a221880475dbd7b
Prateek Panwar <prateekpanwar31@gmail.com>
2018-09-19 21:06:58 -0400

    SQL basics

--- SQL/SQL.py
+++ SQL/SQL.py
@@ -6,6 +6,9 @@ Created on Wed Sep 19 18:56:25 2018
 """
 
 import sqlite3
+import time
+import datetime
+import random
 
 conn = sqlite3.connect('tutorial.db') #create a db
 c = conn.cursor() #creating cursor
@@ -14,10 +17,45 @@ def create_table():
     c.execute('CREATE TABLE IF NOT EXISTS stuffsToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)')
     
 def data_entry():
-    c.execute("INSERT INTO stuffsToPlot VALUES(12345678, '2016-01-01', 'Python', 5)")
+    c.execute("INSERT INTO stuffsToPlot VALUES(12345665478, '2016-01-01', 'Python', 5)")
     conn.commit()
     c.close()
     conn.close()
     
-create_table()
-data_entry()
+def dynamic_data_entry():
+    unix = time.time()
+    date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+    keyword = "Python"
+    value = random.randrange(0,10)
+    c.execute("INSERT INTO stuffsToPlot (unix, datestamp, keyword, value) VALUES (?, ?, ?, ?)",
+              (unix, date, keyword, value))
+    conn.commit()
+
+def read_from_db():
+    #c.execute("SELECT * FROM stuffsToPlot")
+    #c.execute("SELECT * FROM stuffsToPlot WHERE value=3 AND keyword='Python'")
+    c.execute("SELECT unix, datestamp FROM stuffsToPlot WHERE value=8 AND keyword='Python'")
+    #data = c.fetchall()
+    #print(data)                    #getting the data all at once
+    
+    for data in c.fetchall():
+        print(data)
+
+def del_and_update():
+    c.execute("UPDATE stuffsToPlot SET value = 3 WHERE value=8")
+    conn.commit()
+    c.execute("SELECT * FROM stuffsToPlot WHERE value=3")
+    for data in c.fetchall():
+        print(data)
+    
+#create_table() #creating a table
+#data_entry() #manual entry
+#for i in range(20):    
+#    dynamic_data_entry() #Automatic entry
+#    time.sleep(1)
+
+#read_from_db()
+del_and_update()
+c.close()
+conn.close()
+    
